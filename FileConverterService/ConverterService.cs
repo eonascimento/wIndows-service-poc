9233691d11d3437df343eae938beeb7f3e89db86
Ênio Oliveira <eonascimento@stefainini.com>
2020-11-08 01:02:30 -0300

    Add project files.

---
+++ FileConverterService/ConverterService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Topshelf.Logging;
+
+namespace FileConverterService
+{
+    class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+        private static readonly LogWriter _log = HostLogger.Get<ConverterService>();
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"c:\Temp\a","*_in.txt");
+            _watcher.Created += FileCreated;
+
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _log.InfoFormat("Starting conversion of '{0}'",e.FullPath);
+
+            if (e.FullPath.Contains("bad_in"))
+            {
+                throw new NotSupportedException("Cannot convert");
+            }
+
+            string content = File.ReadAllText(e.FullPath);
+
+            string uppperContent = content.ToUpperInvariant();
+
+            var dir = Path.GetDirectoryName(e.FullPath);
+
+            var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+
+            var convertedPath = Path.Combine(dir, convertedFileName);
+
+            File.WriteAllText(convertedPath,uppperContent);
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+            return true;
+        }
+
+        public bool Pause()
+        {
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+        public bool Continue()
+        {
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+        public void CustomCommand(int commandNumber)
+        {
+            _log.InfoFormat("Teste '{0}'",commandNumber);
+        }
+    }
+}
---
+++ FileConverterService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Topshelf;
+
+namespace FileConverterService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.UseNLog();
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                    serviceInstance.WhenPaused(execute => execute.Pause());
+                    serviceInstance.WhenContinued(execute => execute.Continue());
+                    serviceInstance.WhenCustomCommandReceived((execute,hostControl, commandNumber) =>
+                        execute.CustomCommand(commandNumber));
+
+                });
+                serviceConfig.EnableServiceRecovery(recoveryOption =>
+                {
+                    recoveryOption.RestartService(1);
+                    recoveryOption.RestartComputer(60, "Demonstração Serviço Windows");
+                    recoveryOption.RunProgram(5, "c:\\algumprograma.exe");
+                });
+                serviceConfig.EnablePauseAndContinue();
+                serviceConfig.SetServiceName("ServicoWindowsConversorArquivo");
+                serviceConfig.SetDisplayName("Demonstração Serviço Windows");
+                serviceConfig.SetDescription("Serviço do windows para converter arquivo");
+
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
